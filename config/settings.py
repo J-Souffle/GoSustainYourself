28271d579e8f2f4015058881eece8da455fef0b6
eddieX-2021 <eddiexiao2019@gmail.com>
2025-03-29 20:09:04 -0400

    fix conflict

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gosustainyourself',
+        'USER': 'root',       # Same as in ALTER USER
+        'PASSWORD': 'HoosHack1234!',  # Updated password
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 # import mongoengine
