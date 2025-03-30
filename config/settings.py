a8a617e163c42712308ca88f51752952fdfc5c87
Kuthan Kukrer <kuthan@kukrer.com>
2025-03-30 01:53:47 -0400

    made ui changes

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'gosustainyourself.apps.GosustainyourselfConfig',
-    'django_bootstrap5'
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
